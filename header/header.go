745072215abcbc3255da3cff7715ca7d3a700dd9
Hiroya Onoe <git@onoe.dev>
2022-11-16 03:04:30 +0900

    Add header.WriteTo

--- header/header.go
+++ header/header.go
@@ -1,7 +1,22 @@
 package header
 
+import (
+	"bytes"
+	"io"
+)
+
 type Header struct{}
 
 func (h *Header) String() string {
 	return "BOPP TEST"
+}
+
+func (h *Header) Format() []byte {
+	return []byte(h.String())
+}
+
+// WriteTo renders a proxy protocol header in a format and writes it to an io.Writer.
+func (h *Header) WriteTo(w io.Writer) (int64, error) {
+	buf := h.Format()
+	return bytes.NewBuffer(buf).WriteTo(w)
 }
