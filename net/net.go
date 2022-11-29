a8adec48826a80f78f3f157ea390b621289d6cef
Hiroya Onoe <git@onoe.dev>
2022-11-29 16:41:00 +0900

    net.Connのwrapperを実装

--- net/net.go
+++ net/net.go
@@ -3,14 +3,20 @@ package protocol
 import (
 	"bufio"
 	"net"
+	"sync"
 
 	"github.com/hiroyaonoe/bcop-go/header"
 )
 
 type Conn struct {
 	net.Conn
-	header    *header.Header
 	bufReader *bufio.Reader
+	rHeader   *header.Header
+	rOnce     sync.Once
+	rErr      error
+	wHeader   *header.Header
+	wOnce     sync.Once
+	wErr      error
 }
 
 var _ net.Conn = &Conn{}
@@ -18,34 +24,37 @@ var _ net.Conn = &Conn{}
 func NewConn(c net.Conn, h *header.Header) *Conn {
 	return &Conn{
 		Conn:      c,
-		header:    h,
 		bufReader: bufio.NewReader(c),
+		wHeader:   h,
 	}
 }
 
 func (c *Conn) Read(b []byte) (n int, err error) {
-	err = c.readHeader()
-	if err != nil {
-		return 0, err
+	c.readHeader()
+	if c.rErr != nil {
+		return 0, c.rErr
 	}
 
 	return c.bufReader.Read(b)
 }
 
 func (c *Conn) Write(b []byte) (n int, err error) {
-	_, err = c.header.WriteTo(c.Conn)
-	if err != nil {
-		return 0, err
+	c.writeHeader()
+	if c.wErr != nil {
+		return 0, c.wErr
 	}
-	n, err = c.Write(b)
-	return n, err
+
+	return c.Conn.Write(b)
 }
 
-func (c *Conn) readHeader() error {
-	h, err := header.Read(c.bufReader)
-	if err != nil {
-		return err
-	}
-	c.header = h
-	return nil
+func (c *Conn) readHeader() {
+	c.rOnce.Do(func() {
+		c.rHeader, c.rErr = header.Read(c.bufReader)
+	})
+}
+
+func (c *Conn) writeHeader() {
+	c.wOnce.Do(func() {
+		_, c.wErr = c.wHeader.WriteTo(c.Conn)
+	})
 }
